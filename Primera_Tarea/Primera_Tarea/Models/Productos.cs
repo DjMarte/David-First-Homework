3cb1c5410c2f22b681bb140dc918d243481b1936
DjMarte <alphat098@gmail.com>
2024-04-27 00:53:46 -0400

    Creación de la carpeta Models y sus atributos

---
+++ Primera_Tarea/Primera_Tarea/Models/Productos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Primera_Tarea.Models;
+
+public class Productos
+{
+	[Key]
+	public int ProductoId { get; set; }
+
+	[Required(ErrorMessage = "Descripción obligatoria")]
+
+	public string? Descripcion { get; set; }
+
+    [Range(0.1, 10000, ErrorMessage = "Ingrese un precio válido")]
+    [Required(ErrorMessage = "Precio obligatorio")]
+    public decimal Precio { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras")]
+	[Required(ErrorMessage ="Categoria Obligatoria")]
+
+	public string? Categoria { get; set; }
+}
