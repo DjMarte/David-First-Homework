0ce249ff60f259de395be23455cebcca5c61733f
DjMarte <alphat098@gmail.com>
2024-04-27 00:57:33 -0400

    Cree la Carpeta DAL y el Contexto

---
+++ Primera_Tarea/Primera_Tarea/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Primera_Tarea.Models;
+
+namespace Primera_Tarea.DAL
+{
+	public class Contexto : DbContext
+	{
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+            
+        }
+
+        DbSet<Productos> Productos { get; set; }
+    }
+}
